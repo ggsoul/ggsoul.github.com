94f23bf73d858ab0e3a89c7628ed6fa142337c3b
gaoxuefeng <gaoxuefeng@baidu.com>
2017-07-10 16:55:20 +0800

    Site updated: 2017-07-10 16:55:20

--- iqiyi/js/changeAlbum.js
+++ iqiyi/js/changeAlbum.js
@@ -1,45 +1,33 @@
 var videoList = [
     {
-        src: 'https://ss0.bdstatic.com/-b1Caiqa0d9Bmcmop9aC2jh9h2w8e4_h7sED0YQ_t9iCPK/mda-hc3ysacvgnsg4zqj/hd/mda-hc3ysacvgnsg4zqj.mp4?playlist=%5B%22hd%22%5D&auth_key=1497689669-0-0-c671c9a25146624bdb0a40668b8a9a88&bcevod_channel=searchbox_feed',
-        opt: {
-            "id":"8704195909973021667",
-            "commentVersion":"2017244180",
-            "dtid":"",
-            "title":"《加勒比海盗5》最新幕后精彩预告，杰克船长如何改变厄运",
-            "poster":"http://pic.rmb.bdstatic.com/mda-hc3ysacvgnsg4zqj.jpg",
-            "source_from":"trans",
-            "locid":"http://www.internal.video.baidu.com/2f1c1c1c29d346b65cba8b6d95271166.html",
-            "comment_id":"",
-            "shareInfo":{
-                "mediaType":"all",
-                "title":"《加勒比海盗5》最新幕后精彩预告，杰克船长如何改变厄运","content":"好看的视频,打开看看吧！",
-                "type":"url",
-                "source":"baidumedia",
-                "linkUrl":"https://sv.baidu.com/videoui/page/videoland?isBdboxShare=1&context=%7B%22nid%22%3A%22sv_8704195909973021667%22%7D&pd=share",
-                "iconUrl":"http://pic.rmb.bdstatic.com/mda-hc3ysacvgnsg4zqj.jpg",
-                "categoryInfo":{"nid":"sv_8704195909973021667"}
-            }
-        }
+        id: '8704195909973021667',
+        poster: 'http://pic.rmb.bdstatic.com/mda-hc3ysacvgnsg4zqj.jpg',
+        src: 'https://ss0.bdstatic.com/-b1Caiqa0d9Bmcmop9aC2jh9h2w8e4_h7sED0YQ_t9iCPK/mda-hc3ysacvgnsg4zqj/hd/mda-hc3ysacvgnsg4zqj.mp4?playlist=%5B%22hd%22%5D&auth_key=1497689669-0-0-c671c9a25146624bdb0a40668b8a9a88&bcevod_channel=searchbox_feed'
     },
     {
-        src: 'https://vd1.bdstatic.com/mda-hfvebxr9fw53wuyc/mda-hfvebxr9fw53wuyc.mp4?playlist=%5B%22hd%22%5D&auth_key=1497938875-0-0-5bc7b21f76077c65825e93ec17034a40&bcevod_channel=searchbox_feed',
-        opt: {"id":"12653024706116713870","commentVersion":"2017244180","dtid":"10735147359471223911","title":"伐木累将前往捷克，与十位捷克“兄弟”组成的团队来一场“中捷友谊赛”！据说还有一对一超震撼的撕名牌？！精彩刺激一触即发。","poster":"https://ss0.bdstatic.com/9bA1vGfa2gU2pMbfm9GUKT-w/timg?searchbox_feed&quality=100&wh_rate=0&size=b800_1000&ref=http%3A%2F%2Fwww.baidu.com&sec=0&di=2f36eb2d343c14ee4e203aed8b20835d&src=http%3A%2F%2Fb.bdstatic.com%2Fsearchbox%2Ffile%2Fstar%2F20170620%2F1497925866194925.png","source_from":"starvideo","locid":"http://www.internal.video.baidu.com/f338ae1f5b599a7acc12f6ac865f6b7c.html","comment_id":"","shareInfo":{"mediaType":"all","title":"伐木累将前往捷克，与十位捷克“兄弟”组成的团队来一场“中捷友谊赛”！据说还有一对一超震撼的撕名牌？！精彩刺激一触即发。","content":"好看的视频,打开看看吧！","type":"url","source":"baidumedia","linkUrl":"https://sv.baidu.com/videoui/page/videoland?isBdboxShare=1&context=%7B%22nid%22%3A%22sv_12653024706116713870%22%7D&pd=share","iconUrl":"https://ss0.bdstatic.com/9bA1vGfa2gU2pMbfm9GUKT-w/timg?searchbox_feed&quality=100&wh_rate=0&size=b800_1000&ref=http%3A%2F%2Fwww.baidu.com&sec=0&di=2f36eb2d343c14ee4e203aed8b20835d&src=http%3A%2F%2Fb.bdstatic.com%2Fsearchbox%2Ffile%2Fstar%2F20170620%2F1497925866194925.png","categoryInfo":{"nid":"sv_12653024706116713870"}}}
+        id: '12653024706116713870',
+        poster: 'https://ss0.bdstatic.com/9bA1vGfa2gU2pMbfm9GUKT-w/timg?searchbox_feed&quality=100&wh_rate=0&size=b800_1000&ref=http%3A%2F%2Fwww.baidu.com&sec=0&di=2f36eb2d343c14ee4e203aed8b20835d&src=http%3A%2F%2Fb.bdstatic.com%2Fsearchbox%2Ffile%2Fstar%2F20170620%2F1497925866194925.png',
+        src: 'https://vd1.bdstatic.com/mda-hfvebxr9fw53wuyc/mda-hfvebxr9fw53wuyc.mp4?playlist=%5B%22hd%22%5D&auth_key=1497938875-0-0-5bc7b21f76077c65825e93ec17034a40&bcevod_channel=searchbox_feed'
     },
     {
-        src: 'https://vd1.bdstatic.com/mda-hdewiv13quzsqyj7/hd/mda-hdewiv13quzsqyj7.mp4?playlist=%5B%22hd%22%2C%22sc%22%5D&auth_key=1497689426-0-0-5cfd8fcfa3e62a37e94e6b6def4fa28d&bcevod_channel=searchbox_feed',
-        opt: {"id":"11324487133180799594","commentVersion":"2017244180","dtid":"","title":"沙画大师，带你走进不一样的美","poster":"http://vd1.bdstatic.com/mda-hdewiv13quzsqyj7/mda-hdewiv13quzsqyj7.jpg","source_from":"trans","locid":"http://www.internal.video.baidu.com/ae67a7b871ef9b86978b61db999327dc.html","comment_id":"","shareInfo":{"mediaType":"all","title":"沙画大师，带你走进不一样的美","content":"好看的视频,打开看看吧！","type":"url","source":"baidumedia","linkUrl":"https://sv.baidu.com/videoui/page/videoland?isBdboxShare=1&context=%7B%22nid%22%3A%22sv_11324487133180799594%22%7D&pd=share","iconUrl":"http://vd1.bdstatic.com/mda-hdewiv13quzsqyj7/mda-hdewiv13quzsqyj7.jpg","categoryInfo":{"nid":"sv_11324487133180799594"}}}
+        id: '11324487133180799594',
+        poster: 'http://vd1.bdstatic.com/mda-hdewiv13quzsqyj7/mda-hdewiv13quzsqyj7.jpg',
+        src: 'https://vd1.bdstatic.com/mda-hdewiv13quzsqyj7/hd/mda-hdewiv13quzsqyj7.mp4?playlist=%5B%22hd%22%2C%22sc%22%5D&auth_key=1497689426-0-0-5cfd8fcfa3e62a37e94e6b6def4fa28d&bcevod_channel=searchbox_feed'
     },
     {
-        src: 'https://vd1.bdstatic.com/mda-hffgtxaifywk5eaj/mda-hffgtxaifywk5eaj.mp4?auth_key=1497689535-0-0-7ff9fa11c651afd0de7ad9db5ec7a9c1&bcevod_channel=searchbox_feed',
-        opt: {"id":"4605177881513504166","commentVersion":"2017244180","dtid":"","title":"学校才女三人合作《凉凉》 竹笛+古筝+舞蹈  校园版MV","poster":"https://ss0.bdstatic.com/9bA1vGfa2gU2pMbfm9GUKT-w/timg?searchbox_feed&quality=100&wh_rate=0&size=b800_1000&ref=http%3A%2F%2Fwww.baidu.com&sec=0&di=99c1eb35b8f27fe5e6faf64c15caa07e&src=http%3A%2F%2Fbos.nj.bpc.baidu.com%2Fv1%2Fmediaspot%2Fd2b9b132a3daeb032103a7afdb9b5555.jpeg","source_from":"bjh","locid":"http://www.internal.video.baidu.com/a4b1b47e40d015f86a7cd21afd1ba06d.html","comment_id":"1569426012607751","shareInfo":{"mediaType":"all","title":"学校才女三人合作《凉凉》 竹笛+古筝+舞蹈  校园版MV","content":"好看的视频,打开看看吧！","type":"url","source":"baidumedia","linkUrl":"https://sv.baidu.com/videoui/page/videoland?isBdboxShare=1&context=%7B%22nid%22%3A%22sv_4605177881513504166%22%7D&pd=share","iconUrl":"https://ss0.bdstatic.com/9bA1vGfa2gU2pMbfm9GUKT-w/timg?searchbox_feed&quality=100&wh_rate=0&size=b800_1000&ref=http%3A%2F%2Fwww.baidu.com&sec=0&di=99c1eb35b8f27fe5e6faf64c15caa07e&src=http%3A%2F%2Fbos.nj.bpc.baidu.com%2Fv1%2Fmediaspot%2Fd2b9b132a3daeb032103a7afdb9b5555.jpeg","categoryInfo":{"nid":"sv_4605177881513504166"}}}
+        id: '4605177881513504166',
+        poster: 'https://ss0.bdstatic.com/9bA1vGfa2gU2pMbfm9GUKT-w/timg?searchbox_feed&quality=100&wh_rate=0&size=b800_1000&ref=http%3A%2F%2Fwww.baidu.com&sec=0&di=99c1eb35b8f27fe5e6faf64c15caa07e&src=http%3A%2F%2Fbos.nj.bpc.baidu.com%2Fv1%2Fmediaspot%2Fd2b9b132a3daeb032103a7afdb9b5555.jpeg',
+        src: 'https://vd1.bdstatic.com/mda-hffgtxaifywk5eaj/mda-hffgtxaifywk5eaj.mp4?auth_key=1497689535-0-0-7ff9fa11c651afd0de7ad9db5ec7a9c1&bcevod_channel=searchbox_feed'
     },
     {
-        src: 'https://vd1.bdstatic.com/mda-heymq7qgar7r6yhh/hd/mda-heymq7qgar7r6yhh.mp4?playlist=%5B%22hd%22%5D&auth_key=1497689582-0-0-7d4e2588574c13060ce5c31f788223ff&bcevod_channel=searchbox_feed',
-        opt: {"id":"14400512106424409164","commentVersion":"2017244180","dtid":"","title":"灭绝师太仗着倚天剑太嚣张，张三丰打到她怀疑人生","poster":"https://ss0.bdstatic.com/9bA1vGfa2gU2pMbfm9GUKT-w/timg?searchbox_feed&quality=100&wh_rate=0&size=b800_1000&ref=http%3A%2F%2Fwww.baidu.com&sec=0&di=f2e129e8e88ea6a765b544482013f97c&src=http%3A%2F%2Fbos.nj.bpc.baidu.com%2Fv1%2Fmediaspot%2F280e1ee579cd2d56517b729ed3fa7719.jpeg","source_from":"bjh","locid":"http://www.internal.video.baidu.com/96f2edf23e5febc43165e5a9050aeeaf.html","comment_id":"1568170695635325","shareInfo":{"mediaType":"all","title":"灭绝师太仗着倚天剑太嚣张，张三丰打到她怀疑人生","content":"好看的视频,打开看看吧！","type":"url","source":"baidumedia","linkUrl":"https://sv.baidu.com/videoui/page/videoland?isBdboxShare=1&context=%7B%22nid%22%3A%22sv_14400512106424409164%22%7D&pd=share","iconUrl":"https://ss0.bdstatic.com/9bA1vGfa2gU2pMbfm9GUKT-w/timg?searchbox_feed&quality=100&wh_rate=0&size=b800_1000&ref=http%3A%2F%2Fwww.baidu.com&sec=0&di=f2e129e8e88ea6a765b544482013f97c&src=http%3A%2F%2Fbos.nj.bpc.baidu.com%2Fv1%2Fmediaspot%2F280e1ee579cd2d56517b729ed3fa7719.jpeg","categoryInfo":{"nid":"sv_14400512106424409164"}}}
+        id: '14400512106424409164',
+        poster: 'https://ss0.bdstatic.com/9bA1vGfa2gU2pMbfm9GUKT-w/timg?searchbox_feed&quality=100&wh_rate=0&size=b800_1000&ref=http%3A%2F%2Fwww.baidu.com&sec=0&di=f2e129e8e88ea6a765b544482013f97c&src=http%3A%2F%2Fbos.nj.bpc.baidu.com%2Fv1%2Fmediaspot%2F280e1ee579cd2d56517b729ed3fa7719.jpeg',
+        src: 'https://vd1.bdstatic.com/mda-heymq7qgar7r6yhh/hd/mda-heymq7qgar7r6yhh.mp4?playlist=%5B%22hd%22%5D&auth_key=1497689582-0-0-7d4e2588574c13060ce5c31f788223ff&bcevod_channel=searchbox_feed'
     },
     {
-        src: 'https://vd1.bdstatic.com/mda-hdvwmku1dxxic1g1/hd/mda-hdvwmku1dxxic1g1.mp4?playlist=%5B%22hd%22%2C%22sc%22%5D&auth_key=1497689626-0-0-45f57860787dfb5415c8245ebff66588&bcevod_channel=searchbox_feed',
-        opt: {"id":"14725699043277195038","commentVersion":"2017244180","dtid":"","title":"钢琴版《三生三世十里桃花》 超好听","poster":"https://ss0.bdstatic.com/9bA1vGfa2gU2pMbfm9GUKT-w/timg?searchbox_feed&quality=100&wh_rate=0&size=b800_1000&ref=http%3A%2F%2Fwww.baidu.com&sec=0&di=a44d1255959cf4b95f42973e58e1d929&src=http%3A%2F%2Fpic.rmb.bdstatic.com%2Fe8228351fc28b9cf3fc303bb655f6373.jpg","source_from":"trans","locid":"http://www.internal.video.baidu.com/8055c505106f9442081361f21ff03650.html","comment_id":"","shareInfo":{"mediaType":"all","title":"钢琴版《三生三世十里桃花》 超好听","content":"好看的视频,打开看看吧！","type":"url","source":"baidumedia","linkUrl":"https://sv.baidu.com/videoui/page/videoland?isBdboxShare=1&context=%7B%22nid%22%3A%22sv_14725699043277195038%22%7D&pd=share","iconUrl":"https://ss0.bdstatic.com/9bA1vGfa2gU2pMbfm9GUKT-w/timg?searchbox_feed&quality=100&wh_rate=0&size=b800_1000&ref=http%3A%2F%2Fwww.baidu.com&sec=0&di=a44d1255959cf4b95f42973e58e1d929&src=http%3A%2F%2Fpic.rmb.bdstatic.com%2Fe8228351fc28b9cf3fc303bb655f6373.jpg","categoryInfo":{"nid":"sv_14725699043277195038"}}}
+        id: '14725699043277195038',
+        poster: 'https://ss0.bdstatic.com/9bA1vGfa2gU2pMbfm9GUKT-w/timg?searchbox_feed&quality=100&wh_rate=0&size=b800_1000&ref=http%3A%2F%2Fwww.baidu.com&sec=0&di=a44d1255959cf4b95f42973e58e1d929&src=http%3A%2F%2Fpic.rmb.bdstatic.com%2Fe8228351fc28b9cf3fc303bb655f6373.jpg',
+        src: 'https://vd1.bdstatic.com/mda-hdvwmku1dxxic1g1/hd/mda-hdvwmku1dxxic1g1.mp4?playlist=%5B%22hd%22%2C%22sc%22%5D&auth_key=1497689626-0-0-45f57860787dfb5415c8245ebff66588&bcevod_channel=searchbox_feed'
     }
 ];
 
@@ -63,11 +51,11 @@ $(document).ready(function() {
             var videoData = videoList[index % 5];
 
             $('video')[0].src = videoData.src;
-            $('video')[0].poster = videoData.opt.poster;
+            $('video')[0].poster = videoData.poster;
 
             postMessage.post({
                 actionType: 'shareUrl',
-                actionParams: videoData.opt
+                actionParams: videoData.id
             });
         }
     });
